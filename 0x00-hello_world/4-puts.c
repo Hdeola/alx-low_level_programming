5f0a847fb62122f34d9721efc1cd06976827f4ac
Hope Adetukasi <uniquehadeola@gmail.com>
2022-09-10 11:57:08 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 /**
- *main - entry point for the program.
+*main - entry point for the program.
 *
 * Return: 0 if no errors, return non zero if errors
 */
 int main(void)
 {
-
- 	puts("/"Programming is like building a multilingual puzzle");
+	puts("/"Programming is like building a multilingual puzzle");
        	return (0);
 {
