9b6571deab60d6fbd6a5bcb4a0e016e22029e0b6
Hope Adetukasi <uniquehadeola@gmail.com>
2022-09-16 02:15:22 -0700

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,18 +1,25 @@
-#include *main.h*
+#include "main.h"
+
 
 /**
-*-isdigit - checks wheather a character is a digit or not
-*@c: tested character 
-*Return: 1 if it is, 0 otherwise
-*/
+ *  _isdigit - checks whether a  character is a digit or not
+ * @c: tested character
+ * Return: 1 if it is, 0 otherwise
+ */
 
-int -isdigit(int c) 
+int _isdigit(int c)
 {
 	if ((c >= 48) && (c <= 57))
+
 	{
-		return
-(1): 
+
+		return (1);
+
 	}
 
-	Return (0);
+
+
+	return (0);
+
 }
+
